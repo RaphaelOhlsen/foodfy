7622c565e2d7b6839ace9b0f91b564ff7ee34dd8
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-02-07 19:44:24 -0300

    Finished recipe.njk. This is my second version.

--- public/button.js
+++ public/button.js
@@ -1,34 +1,18 @@
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
-const ingredients = document.querySelector('.ingredients');
-const contentPreparation = document.querySelector('#preparation');
-const contentInformation = document.querySelector('#information');
+const showButtons = document.querySelectorAll(".show-button");
 
-button1.addEventListener('click', () => {
-  ingredients.classList.toggle('hidde');
-
-  if (button1.innerHTML === "MOSTRAR") {
+showButtons.forEach(showButton => {
+  showButton.addEventListener('click', () => {
     
-    return button1.innerHTML = "ESCONDER"
-  }
-  return button1.innerHTML = "MOSTRAR"
-})
-
-button2.addEventListener('click', () => {
-  contentPreparation.classList.toggle('hidde');
-
-  if (button2.innerHTML === "MOSTRAR") {
-    return button2.innerHTML = "ESCONDER"
-  }
-  return button2.innerHTML = "MOSTRAR"
-})
-
-button3.addEventListener('click', () => {
-  contentInformation.classList.toggle('hidde');
-  
-  if (button3.innerHTML === "MOSTRAR") {
-    return button3.innerHTML = "ESCONDER"
-  }
-  return button3.innerHTML = "MOSTRAR"
+    if (showButton.innerHTML === "MOSTRAR") {
+      showButton.innerHTML = "ESCONDER";
+    } else {
+      showButton.innerHTML = "MOSTRAR";
+    }
+    const content = showButton.nextElementSibling;
+    if (content.classList.contains("hidde")) {
+      content.classList.remove("hidde")
+    } else {
+      content.classList.add("hidde");
+    }
+  })
 })
